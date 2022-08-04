7b6bde8aaa79daf3112209781f7d0e9b1adf364f
baidada <1374673998@qq.com>
2022-08-04 11:19:15 +0800

    公共部分完成

---
+++ assets/js/index.js
@@ -0,0 +1,44 @@
+const getUserInfo = () => {
+  $.ajax({
+    type: "GET",
+    url: "/my/userinfo",
+    data: null,
+    headers: {
+      Authorization: localStorage.getItem("token"),
+    },
+    success: (res) => {
+        // console.log(res);
+      const {message,status} = res
+      if(status !== 0) return layer.msg(message)
+      renderAvatar(res.data)
+
+    },
+  });
+};
+const renderAvatar = (data) => {
+  let name = data.nickname || data.username;
+  console.log(name);
+  $("#welcome").html("欢迎" + name);
+  if (data.user_pic !== null) {
+    $(".layui-nav-img").attr("src", data.user_pic);
+    $(".text-avatar").hide();
+  } else {
+    $(".layui-nav-img").hide();
+    let firstName = name[0].toUpperCase();
+    $(".text-avatar").html(firstName);
+  }
+};
+getUserInfo();
+
+$("#btnLogout").click(() => {
+    layui.layer.confirm(
+        "确定退出登录？",
+        { icon: 3, title: "" },
+        function (index) {
+            // 清空本地存储里面的 token
+            localStorage.removeItem("token");
+            // 重新跳转到登录页面
+            location.href = "/login.html";
+        }
+    );
+});
